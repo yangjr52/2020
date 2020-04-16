35a25d47b9a07931d7126a853e654e71221eb1a4
YANGJIARUI0502 <760604648@qq.com>
2020-04-17 01:02:41 +0800

    changeImage

---
+++ 图片轮播/test.js
@@ -0,0 +1,65 @@
+//封装一个代替getElementById的方法
+function byId(id){
+	return typeof(id)==="string"?document.getElementById(id):id;
+}
+
+//全局变量
+var index=0,
+	timer=null,
+	pics=byId("banner").getElementsByTagName("div"),
+	dots=byId("dots").getElementsByTagName("span"),
+	len=pics.length;
+
+function slideImg(){
+	var main = byId("main");
+	//滑过清除定时器，离开继续
+	main.onmouseover = function(){
+		//滑过清除定时器
+		if(timer) clearInterval(timer);
+
+	}
+	main.onmouseout=function(){
+		//间歇调用，每隔一段时间调用一次
+		timer=setInterval(function(){
+			index++;
+			if(index>=len){
+				index=0;
+			}
+			//切换图片
+			changeImg();
+		},2000);//每隔2秒钟执行一下里面的脚本
+	}
+
+	//自动在main上触发onmouseout的事件
+	main.onmouseout();
+
+	//点击圆点切换图片，且绑定点击圆点事件，点击圆点切换图片
+	for(var d=0;d<len;d++){
+		//给所有span添加一个id的属性，值为d,作为当前span索引
+		dots[d].id=d;
+		dots[d].onclick=function(){
+			//改变index为当前span的id值
+			alert(this.id);//this指的是绑定在哪个事件，在此处为spand
+
+		}
+		/*	
+			无法使用：function会改变作用域，打印出来是3
+			dots[d].onclick=function(){
+			//改变index为当前span索引
+			index=d;
+		}*/
+	}
+}
+//切换图片
+function changeImg(){
+	//pics是一个数组，className是重写类 不建议写
+	//pics[index].className=''
+	//遍历banner下所有div将其隐藏
+	for(var i=0;i<len;i++){
+		pics[i].style.display='none';
+	}
+	//根据索引
+	pics[index].style.display='block';
+}
+
+slideImg();

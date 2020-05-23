bdca20b34e9047ef7585305305d8d1182b4e08d2
YANGJIARUI0502 <760604648@qq.com>
2020-05-23 23:45:36 +0800

    viewpages

--- 大一_前端练习/平时练习/ToDoList/ToDoList.js
+++ 大一_前端练习/平时练习/ToDoList/ToDoList.js
@@ -23,7 +23,7 @@ window.onload=function(){
 				sec.value=input.value;
 				input.value="";
 
-				btn.onclick=function(){
+			btn.onclick=function(){
 				thi.value=input.value;
 				input.value="";
 

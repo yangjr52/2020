81f0909666499928d53dda65d7206075e0c9824c
YANGJIARUI0502 <760604648@qq.com>
2020-05-26 16:16:10 +0800

    navigationBar.6

--- 大一_前端练习/三级导航栏/navigationBar.js
+++ 大一_前端练习/三级导航栏/navigationBar.js
@@ -7,24 +7,58 @@ var login=byId("login"),
 	p=byId("p"),
 	title1=byId("title1"),
 	title2=byId("title2"),
-	title3=byId("title3");
+	title3=byId("title3"),
+	all=byId("all"),
+	content=all.getElementsByClassName("content"),
+	len=content.length,
+	box1=byId("box1"),
+	box2=byId("box2"),
+	box3=byId("box3");
 
 login.onclick=function(){
 	var name=prompt("用户名：");
 	login.innerHTML='用户：'+name;
 }
 
-blog.onclick=function(){
-	p.innerHTML='博客首页';
-	title1.innerHTML='热门推荐';
-	title2.innerHTML='经典教程';
-	title3.innerHTML='优秀案例';
-	blog.classList.add("active");
-	video.classList.remove("active");
-	title1.classList.add("active2");
-	title2.classList.remove("active2");
-	title3.classList.remove("active2");
-}
+	title1.onclick=function(){
+		title1.classList.add("active2");
+		title2.classList.remove("active2");
+		title3.classList.remove("active2");
+
+		box1.classList.remove("none");
+		box1.classList.add("block");
+		box2.classList.add("none");
+		box2.classList.remove("block");
+		box3.classList.add("none");
+		box3.classList.remove("block");
+	}
+
+	title2.onclick=function(){
+		title2.classList.add("active2");
+		title1.classList.remove("active2");
+		title3.classList.remove("active2");
+
+		box2.classList.remove("none");
+		box2.classList.add("block");
+		box1.classList.add("none");
+		box1.classList.remove("block");
+		box3.classList.add("none");
+		box3.classList.remove("block");
+	}
+
+	title3.onclick=function(){
+		title3.classList.add("active2");
+		title1.classList.remove("active2");
+		title2.classList.remove("active2");
+
+		box3.classList.remove("none");
+		box3.classList.add("block");
+		box1.classList.add("none");
+		box1.classList.remove("block");
+		box2.classList.add("none");
+		box2.classList.remove("block");
+	}
+
 
 video.onclick=function(){
 	p.innerHTML='视频教程';
@@ -38,20 +72,27 @@ video.onclick=function(){
 	title3.classList.remove("active2");
 }
 
-title1.onclick=function(){
+blog.onclick=function(){
+	p.innerHTML='博客首页';
+	title1.innerHTML='热门推荐';
+	title2.innerHTML='经典教程';
+	title3.innerHTML='优秀案例';
+	
+	blog.classList.add("active");
+	video.classList.remove("active");
 	title1.classList.add("active2");
 	title2.classList.remove("active2");
 	title3.classList.remove("active2");
+
+	box1.classList.remove("none");
+		box1.classList.add("block");
+		box2.classList.add("none");
+		box2.classList.remove("block");
+		box3.classList.add("none");
+		box3.classList.remove("block");
 }
 
-title2.onclick=function(){
-	title2.classList.add("active2");
-	title1.classList.remove("active2");
-	title3.classList.remove("active2");
-}
 
-title3.onclick=function(){
-	title3.classList.add("active2");
-	title1.classList.remove("active2");
-	title2.classList.remove("active2");
-}
+
+
+

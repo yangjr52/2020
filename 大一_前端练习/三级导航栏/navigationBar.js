ab76ac70086cd775d30a4a1ce34e7fca4d0f8b46
YANGJIARUI0502 <760604648@qq.com>
2020-05-31 16:25:05 +0800

    navigationBar.8

--- 大一_前端练习/三级导航栏/navigationBar.js
+++ 大一_前端练习/三级导航栏/navigationBar.js
@@ -5,65 +5,80 @@ function byId(id){
 var login=byId("login"),
 	blog=byId("blog"),
 	p=byId("p"),
+	all=byId("all"),
+	banner=byId("banner"),
+
 	title1=byId("title1"),
 	title2=byId("title2"),
 	title3=byId("title3"),
-	all=byId("all"),
-	content=all.getElementsByClassName("content"),
-	len=content.length,
+	title=all.getElementsByClassName("s-title"),
+	lenT=title.length,
+
 	box1=byId("box1"),
 	box2=byId("box2"),
 	box3=byId("box3"),
-	i=0;
+	box=all.getElementsByClassName("content"),
+	lenB=box.length,
+
+	index=0,
+	pics=byId("banner").getElementsByTagName("img"),
+	prev=byId("prev"),
+	next=byId("next"),
+	lenP=pics.length;
 
 login.onclick=function(){
 	var name=prompt("用户名：");
 	login.innerHTML='用户：'+name;
 }
 
-if(i==0){
 	title1.onclick=function(){
+
+		for(var i=0;i<lenT;i++){
+			title[i].classList.remove("active2");
+		}
+		
 		title1.classList.add("active2");
-		title2.classList.remove("active2");
-		title3.classList.remove("active2");
+
+		for(var j=0;j<lenB;j++){
+			box[j].classList.add("none");
+			box[j].classList.remove("block");
+		}
 
 		box1.classList.remove("none");
 		box1.classList.add("block");
-		box2.classList.add("none");
-		box2.classList.remove("block");
-		box3.classList.add("none");
-		box3.classList.remove("block");
 	}
 
 	title2.onclick=function(){
+		for(var i=0;i<lenT;i++){
+			title[i].classList.remove("active2");
+		}
+		
 		title2.classList.add("active2");
-		title1.classList.remove("active2");
-		title3.classList.remove("active2");
+
+		for(var j=0;j<lenB;j++){
+			box[j].classList.add("none");
+			box[j].classList.remove("block");
+		}
 
 		box2.classList.remove("none");
 		box2.classList.add("block");
-		box1.classList.add("none");
-		box1.classList.remove("block");
-		box3.classList.add("none");
-		box3.classList.remove("block");
 	}
 
 	title3.onclick=function(){
+		for(var i=0;i<lenT;i++){
+			title[i].classList.remove("active2");
+		}
+		
 		title3.classList.add("active2");
-		title1.classList.remove("active2");
-		title2.classList.remove("active2");
+
+		for(var j=0;j<lenB;j++){
+			box[j].classList.add("none");
+			box[j].classList.remove("block");
+		}
 
 		box3.classList.remove("none");
 		box3.classList.add("block");
-		box1.classList.add("none");
-		box1.classList.remove("block");
-		box2.classList.add("none");
-		box2.classList.remove("block");
 	}
-}
-else {
-	
-}
 
 
 
@@ -74,41 +89,99 @@ video.onclick=function(){
 	title3.innerHTML='视频投稿';
 	video.classList.add("active");
 	blog.classList.remove("active");
-	title1.classList.add("active2");
-	title2.classList.remove("active2");
-	title3.classList.remove("active2");
 
-		box1.classList.add("none");
-		box1.classList.remove("block");
-		box2.classList.add("none");
-		box2.classList.remove("block");
-		box3.classList.add("none");
-		box3.classList.remove("block");
+
+		for(var i=0;i<lenT;i++){
+			title[i].classList.remove("active2");
+		}
+		
+		title1.classList.add("active2");
+
+		for(var j=0;j<lenB;j++){
+			box[j].classList.add("none");
+			box[j].classList.remove("block");
+		}
+
+		box4.classList.remove("none");
+		box4.classList.add("block");
 }
 
 blog.onclick=function(){
 	p.innerHTML='博客首页';
 	title1.innerHTML='热门推荐';
-	title2.innerHTML='经典教程';
+	title2.innerHTML='博客分类';
 	title3.innerHTML='优秀案例';
 	
 	blog.classList.add("active");
 	video.classList.remove("active");
-	title1.classList.add("active2");
-	title2.classList.remove("active2");
-	title3.classList.remove("active2");
+		
+		for(var i=0;i<lenT;i++){
+			title[i].classList.remove("active2");
+		}
+		
+		title1.classList.add("active2");
+
+		for(var j=0;j<lenB;j++){
+			box[j].classList.add("none");
+			box[j].classList.remove("block");
+		}
 
 		box1.classList.remove("none");
 		box1.classList.add("block");
-		box2.classList.add("none");
-		box2.classList.remove("block");
-		box3.classList.add("none");
-		box3.classList.remove("block");
-
-		i=1000;
 }
 
+function slideImg(){
+	var banner= byId("banner");
+	banner.onmouseover = function(){
+		if(timer) 
+			clearInterval(timer);
+	}
+
+	banner.onmouseout=function(){
+		timer=setInterval(function(){
+			index++;
+			if(index>=lenP){
+				index=0;
+			}
+			changeImg();
+		},2000);
+	}
+}
+
+function changeImg(){
+	for(var i=0;i<lenP;i++){
+		pics[i].style.display='none';
+	}
+	pics[index].style.display='block';
+
+}
+
+slideImg();
+banner.onmouseout();
 
 
+	next.onclick=function(){
+		index++;
+		if(index>=lenP)
+		{
+			index=0;
+		}
+		changeImg(index);
+	}
 
+	prev.onclick=function(){
+		index--;
+		if(index<0)
+		{
+			index=2;
+		}
+		changeImg(index);
+	}
 
+function changeImg(){
+	for(var c=0;c<lenP;c++){
+		pics[c].style.display='none';
+	}
+	pics[index].style.display='block';
+
+}

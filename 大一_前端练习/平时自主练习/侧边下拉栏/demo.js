4b4023326436687871cec4e0a80e1d6b05ff914b
YANGJIARUI0502 <760604648@qq.com>
2020-07-28 22:46:46 +0800

    dropDownBox.3

--- 大一_前端练习/平时自主练习/侧边下拉栏/demo.js
+++ 大一_前端练习/平时自主练习/侧边下拉栏/demo.js
@@ -22,18 +22,37 @@ var lastIndex=0;//上一次点击的下标
 
 for(var i=0;i<aList.length;i++){
 	/*自定义事件*/
-	aList[i].index=i;/*增加了一个属性 用于保存i*/
+	aList[i].index=i;	//增加了一个属性 用于保存i
+	aList[i].isClick=false;		//定义一个开关，判断自己是否被点击
+	/*
+		aList[0].index = 0;
+		aList[1].index = 1;
+	*/
 	aList[i].onclick=function(){
-		//清除上一次样式
+		if(this.isClick){//this.isClick == true;  我被点击了
+			aHide[this.index].style.height='0';
+			aList[this.index].className='';
+			aIcon[this.index].className='';
+			this.isClick=false;//我当前处于未点击状态
+		}
+		else{	//没有被点击
+			
+			/*清除上一次样式*/
 		aHide[lastIndex].style.height='0';
 		aList[lastIndex].className='';
 		aIcon[lastIndex].className='';
 
-		//设置当前样式
+		/*设置当前样式*/
 		aHide[this.index].style.height='245px';
 		aList[this.index].className='on';
 		aIcon[this.index].className='on';
-		lastIndex=this.index; /*保存上一次的下标*/
+		this.isClick=true;	//我现在被点击了
+		lastIndex=this.index; 	//保存上一次的下标
+
+		}
+
+
+		
 	}
 }
 

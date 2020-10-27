991eceed2f3e5ac780ffa585e9726f9e9021007b
YANGJIARUI0502 <760604648@qq.com>
2020-10-27 18:00:51 +0800

    js-tab

--- 前端练习/平时自主练习/tab栏切换/js/tab.js
+++ 前端练习/平时自主练习/tab栏切换/js/tab.js
@@ -3,29 +3,39 @@ class Tab {
     constructor(id){
         //获取元素
         this.main=document.querySelector(id);
+
+        //获取所有的li和section 可以成数组
         this.lis=this.main.querySelectorAll('li');  //导航栏
         this.sections=this.main.querySelectorAll('section');  //内容栏
+
         this.add=this.main.querySelector('.tabadd');
+
         // li的父元素
         this.ul=this.main.querySelector('.firstnav ul:first-child');  //cs3选择器
         // section的父元素
         this.fsection=this.main.querySelector('.tabscon');
+
+        //最下面new了一下tab 就会自动调用
         this.init();  //记得加this
         //that里面保存了construction里面的this
         that = this ;
     }
-    init(){//init 初始化操作，让相关元素绑定事件
+
+    init(){
+        //init 初始化操作，让相关元素绑定事件
+
         this.updateNode();  //重新编号调用
 
         this.add.onclick=this.addTab;
+
         for(var i=0;i<this.lis.length;i++){
-            this.lis[i].index = i;
+            this.lis[i].index = i;  //获取编号
             this.lis[i].onclick = this.toggleTab; // 不要加小括号，加了的话就会直接调用了；
             
         }
     }
 
-    //获取所有的li和section
+    //再次获取所有的li和section
     updateNode(){
         this.lis=this.main.querySelectorAll("li");
         this.sections=this.main.querySelectorAll('section');
@@ -39,6 +49,7 @@ class Tab {
        // this.sections[this.index].className = 'conactive';  //这里的this指向的是this.lis,lis下面并没有sections这个属性 -> 报错
        that.sections[this.index].className = 'conactive';
     }
+
     //清除li和section里面的所有类
     clearClass(){  // this指向的是这个函数的调用者 是this调用的
         for(var i=0;i<this.lis.length;i++){
@@ -58,7 +69,8 @@ class Tab {
         that.ul.insertAdjacentHTML('beforeend',li);  //追加到其余子元素的最后面 详情可参考网页：https://developer.mozilla.org/zh-CN/docs/Web/API/Element/insertAdjacentHTML
         that.fsection.insertAdjacentHTML('beforeend',section);
 
-        that.init();  //重新编号调用
+        that.init();
+
     }
 
     //3.删除功能
@@ -67,4 +79,9 @@ class Tab {
     //4.修改功能
     editTab(){}
 }
+
 var tab = new Tab('#tab');
+
+
+
+//该第15
